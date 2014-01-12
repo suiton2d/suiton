0d0ab18a56490170a84889ea4e77feec52e7487c
jonbonazza <jonbonazza@gmail.com>
2014-01-11 20:58:55 -0500

    moved forgeComponentContentPanel() implementation from Renderer to PanelRenderer
    fixed bug in forgeComponentContentPanel() where the add anaimtion button was not disabled when not texture path was entered.

--- editor/src/com/nebula2d/editor/framework/components/PanelRenderer.java
+++ editor/src/com/nebula2d/editor/framework/components/PanelRenderer.java
@@ -1,17 +1,22 @@
 package com.nebula2d.editor.framework.components;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.nebula2d.editor.framework.GameObject;
+import com.nebula2d.editor.framework.assets.Texture;
+import com.nebula2d.editor.ui.ComponentsDialog;
 import com.nebula2d.editor.ui.ImagePanel;
+import com.nebula2d.editor.ui.NewAnimationPopup;
 
 import javax.swing.*;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
+import javax.swing.event.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class PanelRenderer extends Renderer {
 
@@ -54,6 +59,172 @@ public class PanelRenderer extends Renderer {
                         parent.getRotation());
             }
         }
+    }
+
+    @Override
+    public JPanel forgeComponentContentPanel(final ComponentsDialog parent) {
+
+        final ImagePanel imagePanel = new ImagePanel();
+
+        final JButton addButton = new JButton("Add");
+        addButton.setEnabled(false);
+        final JButton removeButton = new JButton("Remove");
+        removeButton.setEnabled(false);
+        final JTextField imageTf = new JTextField(20);
+
+        if (texture != null) {
+            imageTf.setText(texture.getPath());
+            imagePanel.setImage(getTexture().getPath());
+        }
+        final JButton browseBtn = new JButton("...");
+        browseBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                final JFileChooser fc = new JFileChooser();
+                fc.setDialogTitle("Select a file.");
+
+                if (fc.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+                    final String path = fc.getSelectedFile().getAbsolutePath();
+                    imageTf.setText(path);
+                    imagePanel.setImage(path);
+                    addButton.setEnabled(true);
+                    Gdx.app.postRunnable(new Runnable() {
+                        @Override
+                        public void run() {
+                            PanelRenderer.this.texture = new Texture(path);
+                        }
+                    });
+                    imagePanel.getParent().revalidate();
+                }
+            }
+        });
+
+        final DefaultListModel<Animation> listModel = new DefaultListModel<Animation>();
+        for (Animation anim : getAnimations()) {
+            listModel.addElement(anim);
+        }
+        final JList<Animation> animationList = new JList<Animation>();
+        animationList.setModel(listModel);
+        final JScrollPane sp = new JScrollPane(animationList);
+        sp.setPreferredSize(new Dimension(200, 300));
+
+        addButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new NewAnimationPopup(PanelRenderer.this, listModel, imageTf.getText()).
+                        show(addButton, -1, addButton.getHeight());
+
+            }
+        });
+
+        removeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Animation anim = animationList.getSelectedValue();
+                removeAnimation(anim);
+                listModel.removeElement(anim);
+            }
+        });
+
+        animationList.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                Animation anim = animationList.getSelectedValue();
+
+                removeButton.setEnabled(anim != null);
+            }
+        });
+
+        animationList.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getButton() == MouseEvent.BUTTON1 && e.getClickCount() == 2) {
+                    Animation animation = animationList.getSelectedValue();
+                    animation.showAnimationEditDialog();
+                }
+                super.mouseClicked(e);
+            }
+        });
+        final JPanel addRemoveBtnPanel = new JPanel();
+        addRemoveBtnPanel.add(addButton);
+        addRemoveBtnPanel.add(removeButton);
+
+        final JPanel animationPanel = new JPanel(new BorderLayout());
+        animationPanel.add(addRemoveBtnPanel, BorderLayout.NORTH);
+        animationPanel.add(sp);
+
+        final JLabel imageLbl = new JLabel("Texture:");
+        final JLabel nameLbl = new JLabel("Name:");
+        final JTextField nameTf = new JTextField(20);
+        nameTf.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                String text = nameTf.getText().trim();
+                if (!text.equals("")) {
+                    name = text;
+                    parent.getComponentList().updateUI();
+                }
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                String text = nameTf.getText().trim();
+                if (!text.equals("")) {
+                    name = text;
+                    parent.getComponentList().updateUI();
+                }
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                //To change body of implemented methods use File | Settings | File Templates.
+            }
+        });
+
+        nameTf.setText(name);
+        final JCheckBox enabledCb = new JCheckBox("Enabled");
+        enabledCb.setSelected(enabled);
+        enabledCb.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                enabled = enabledCb.isSelected();
+            }
+        });
+        JPanel leftPanel = new JPanel();
+        GroupLayout layout = new GroupLayout(leftPanel);
+        leftPanel.setLayout(layout);
+
+        layout.setAutoCreateContainerGaps(true);
+        layout.setAutoCreateGaps(true);
+
+        GroupLayout.SequentialGroup hGroup = layout.createSequentialGroup();
+        hGroup.addGroup(layout.createParallelGroup().addComponent(nameLbl).
+                addComponent(imageLbl).addComponent(enabledCb));
+        hGroup.addGroup(layout.createParallelGroup().addComponent(nameTf).addComponent(imageTf));//.
+        //addComponent(imagePanel));
+        hGroup.addGroup(layout.createParallelGroup().addComponent(browseBtn));
+        //hGroup.addGroup(layout.createParallelGroup().addComponent(animationPanel));
+        layout.setHorizontalGroup(hGroup);
+
+        GroupLayout.SequentialGroup vGroup = layout.createSequentialGroup();
+        vGroup.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE).addComponent(nameLbl).
+                addComponent(nameTf));//.addComponent(animationPanel));
+        vGroup.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE).addComponent(imageLbl).
+                addComponent(imageTf).addComponent(browseBtn));
+        vGroup.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE).addComponent(enabledCb));
+        //vGroup.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE).addComponent(imagePanel).
+        //        addComponent(animationPanel));
+        layout.setVerticalGroup(vGroup);
+        final JPanel bottomPanel = new JPanel(new BorderLayout());
+        bottomPanel.add(Box.createRigidArea(new Dimension(100, 0)));
+        bottomPanel.add(animationPanel, BorderLayout.EAST);
+        bottomPanel.add(imagePanel);
+
+        final JPanel mainPanel = new JPanel(new BorderLayout());
+
+        mainPanel.add(leftPanel, BorderLayout.NORTH);
+        mainPanel.add(bottomPanel);
+        return mainPanel;
     }
     //endregion
 }
