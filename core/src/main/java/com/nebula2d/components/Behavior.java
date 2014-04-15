9abc4cf9d61a1a7e7bf303a0612141edbc148152
jonbonazza <jonbonazza@gmail.com>
2014-04-14 22:17:34 -0700

    Behavior core implementation

--- core/src/main/java/com/nebula2d/components/Behavior.java
+++ core/src/main/java/com/nebula2d/components/Behavior.java
@@ -1,7 +1,12 @@
 package com.nebula2d.components;
 
-import com.nebula2d.assets.Asset;
+import com.nebula2d.assets.AssetManager;
 import com.nebula2d.assets.Script;
+import com.sun.org.omg.SendingContext._CodeBaseStub;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Function;
+import org.mozilla.javascript.Scriptable;
+import org.mozilla.javascript.ScriptableObject;
 
 /**
  * Behavior is a {@link com.nebula2d.components.Component} for executing
@@ -13,6 +18,9 @@ import com.nebula2d.assets.Script;
 public class Behavior extends Component {
 
     private Script script;
+    private Scriptable scope;
+    private Function startFunction;
+    private Function updateFunction;
 
     public Behavior(String name, String scriptPath) {
         super(name);
@@ -21,16 +29,37 @@ public class Behavior extends Component {
     }
 
     private void initScript() {
-
+        Context context = Context.enter();
+        try {
+            ScriptableObject globalScope = AssetManager.getInstance().getGlobalScriptScope();
+            scope = context.newObject(globalScope);
+            scope.setPrototype(globalScope);
+            scope.setParentScope(null);
+            context.evaluateString(scope, script.getContents(), script.getFilename(), 1, null);
+            startFunction = (Function) scope.get("start", scope);
+            updateFunction = (Function) scope.get("update", scope);
+        } finally {
+            Context.exit();
+        }
     }
 
     @Override
     public void start() {
-
+        Context context = Context.enter();
+        try {
+            startFunction.call(context, scope, scope, new Object[]{gameObject});
+        } finally {
+            Context.exit();
+        }
     }
 
     @Override
     public void update(float dt) {
-
+        Context context = Context.enter();
+        try {
+            updateFunction.call(context, scope, scope, new Object[]{gameObject, dt});
+        } finally {
+            Context.exit();
+        }
     }
 }
