7845ae5d1b13958e16bf6e03686ae4c4ef708eed
Jon Bonazza <jonbonazza@gmail.com>
2015-07-29 21:42:49 -0700

    TiledTileSheet now has its bounds set when created.

--- core/src/main/java/com/suiton2d/assets/TiledTileSheet.java
+++ core/src/main/java/com/suiton2d/assets/TiledTileSheet.java
@@ -1,6 +1,8 @@
 package com.suiton2d.assets;
 
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileSet;
 
 /**
  * @author Jon Bonazza <jonbonazza@gmail.com
@@ -12,6 +14,19 @@ public class TiledTileSheet extends TileSheet<TiledMap> {
 
     public TiledTileSheet(String path, TiledMap tileMap) {
         super(path, tileMap, TileSheetType.TILED);
+        calculateBounds();
+    }
+
+    private void calculateBounds() {
+        for (TiledMapTileSet tileSet : data.getTileSets()) {
+            for (int i = 0; i < tileSet.size(); ++i) {
+                TextureRegion region = tileSet.getTile(i).getTextureRegion();
+                if (region.getRegionWidth() > width)
+                    width = region.getRegionWidth();
+                if (region.getRegionHeight() > height)
+                    height = region.getRegionHeight();
+            }
+        }
     }
 
     public float getUnitScale() {
